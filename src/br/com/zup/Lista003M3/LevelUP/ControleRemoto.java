6883dc168afad83de427d677d221462d9cd423da
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-09-22 22:34:30 -0300

    Creando Base Ejercicio encapsulamento Controle Remoto

--- src/br/com/zup/Lista003M3/LevelUP/ControleRemoto.java
+++ src/br/com/zup/Lista003M3/LevelUP/ControleRemoto.java
@@ -1,6 +1,40 @@
 package br.com.zup.Lista003M3.LevelUP;
 
 public class ControleRemoto {
-    Televisor tv = new Televisor(0,1);
+    Televisor tv;
 
+    public ControleRemoto(Televisor tv) {
+        this.tv = tv;
+    }
+
+
+    public void aumentarVolumen(int aumentar) {
+
+        aumentar = tv.getVolume() + 1;
+        tv.setVolume(aumentar);
+    }
+
+    public void diminuirVolumen(int diminuir) {
+        diminuir = tv.getVolume()-1;
+        tv.setVolume(diminuir);
+    }
+
+    public void avancarCanal(int canal){
+        canal = tv.getCanal()+1;
+        tv.setCanal(canal);
+    }
+    public void regresarCanal(int canal){
+
+        canal = tv.getCanal()-1;
+        tv.setCanal(canal);
+    }
+    public void trocarCanal(int trocar){
+        tv.setCanal(trocar);
+    }
+
+
+    public void consultar() {
+        System.out.println("Volume: " + tv.getVolume());
+        System.out.println("Canal: " + tv.getCanal());
+    }
 }
--- src/br/com/zup/Lista003M3/LevelUP/Main.java
+++ src/br/com/zup/Lista003M3/LevelUP/Main.java
@@ -1,4 +1,24 @@
 package br.com.zup.Lista003M3.LevelUP;
 
 public class Main {
+    public static void main(String[] args) {
+        Televisor lg = new Televisor(0,1);
+       ControleRemoto cr = new ControleRemoto(lg);
+       lg.getVolume();
+       lg.getCanal();
+        System.out.println("Canal "+ lg.getCanal()+" Volume "+ lg.getVolume());
+        cr.aumentarVolumen(1);
+        cr.consultar();
+        cr.aumentarVolumen(1);
+        cr.aumentarVolumen(1);
+        cr.consultar();
+        cr.diminuirVolumen(1);
+        cr.consultar();
+
+
+
+
+
+
+    }
 }
