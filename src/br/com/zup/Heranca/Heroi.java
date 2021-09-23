bddebf0f6072e0f16211e94d0ce9f172d3b802e1
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-09-23 11:51:06 -0300

    Creando constructores e Getters-Setters

--- src/br/com/zup/Heranca/Heroi.java
+++ src/br/com/zup/Heranca/Heroi.java
@@ -4,5 +4,25 @@ public class Heroi {
     //Atributos
     String nome;
     double vida;
+    //Metodo Construtor
+    public Heroi(String nome, double vida) {
+        this.nome = nome;
+        this.vida = vida;
+    }
+    // Getters - Setters
+    public String getNome() {
+        return nome;
+    }
 
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getVida() {
+        return vida;
+    }
+
+    public void setVida(double vida) {
+        this.vida = vida;
+    }
 }
