f47e47d072c0a270ab492be0ab00769bab6693c2
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-09-24 14:53:57 -0300

    Adicionando Classe Administrador - Calcular Salario

--- src/br/com/zup/HerancaPolimorfismo/Main.java
+++ src/br/com/zup/HerancaPolimorfismo/Main.java
@@ -4,8 +4,12 @@ public class Main {
     public static void main(String[] args) {
         Forcenador nike = new Forcenador("Nike", "Silicon Valley", "123456", 1500,350);
         Colaborador maria = new Colaborador("Maria", "rua Le vile", "789456", 156, 1050, 20);
+        Administrador luc = new Administrador("Lucia"," Rua Aponguao", "456789", 162,1250,500,150);
+
+        System.out.println(luc.calcularSalario());
         System.out.println(nike.obterSaldo());
         System.out.println(maria.calcularSalario());
+
     }
 
 
