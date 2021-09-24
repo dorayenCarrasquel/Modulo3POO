6cc3033718382b411f90f14b3c542774c600abf4
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-09-24 15:28:41 -0300

    Adicionando Super Classe Funcionarios

---
+++ src/br/com/zup/ClassePolimorfismo/Funcionarios.java
@@ -0,0 +1,60 @@
+package br.com.zup.ClassePolimorfismo;
+
+public class Funcionarios {
+    private String nome;
+    private String cpf;
+    private String numeroDeRegistro;
+    private String orgaoDeLotacao;
+    private double salario;
+
+public Funcionarios(){
+
+}
+    public Funcionarios(String nome, String cpf, String numeroDeRegistro, String orgaoDeLotacao, double salario) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.numeroDeRegistro = numeroDeRegistro;
+        this.orgaoDeLotacao = orgaoDeLotacao;
+        this.salario = salario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNumeroDeRegistro() {
+        return numeroDeRegistro;
+    }
+
+    public void setNumeroDeRegistro(String numeroDeRegistro) {
+        this.numeroDeRegistro = numeroDeRegistro;
+    }
+
+    public String getOrgaoDeLotacao() {
+        return orgaoDeLotacao;
+    }
+
+    public void setOrgaoDeLotacao(String orgaoDeLotacao) {
+        this.orgaoDeLotacao = orgaoDeLotacao;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+}
