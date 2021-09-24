c82b6e72f439f87ef1c7947cad59b84b8b58a1ef
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-09-24 10:12:36 -0300

    Testando no Fornecedor - Obter Saldo()

---
+++ src/br/com/zup/HerancaPolimorfismo/Main.java
@@ -0,0 +1,12 @@
+package br.com.zup.HerancaPolimorfismo;
+
+public class Main {
+    public static void main(String[] args) {
+        Forcenador nike = new Forcenador("nike", "Silicon Valley", "123456", 1500,350);
+        System.out.println(nike.obterSaldo());
+    }
+
+
+
+
+}
