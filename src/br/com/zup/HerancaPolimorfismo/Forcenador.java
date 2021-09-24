5dccac7f3836d7c3db29b4dd59ce4dbde74b6114
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-09-24 09:28:41 -0300

    Adicionando Métodos Base na Classe Fornecedor

---
+++ src/br/com/zup/HerancaPolimorfismo/Forcenador.java
@@ -0,0 +1,44 @@
+package br.com.zup.HerancaPolimorfismo;
+
+public class Forcenador extends Pessoa {
+    //Atributos
+
+    private double valorCredito;
+    private double valorDivida;
+
+    //Metodo Construtor
+
+    public Forcenador(){
+
+    }
+    public Forcenador(String nome, String endereco, String telefone, double valorCredito, double valorDivida) {
+        super(nome, endereco, telefone);
+        this.valorCredito = valorCredito;
+        this.valorDivida = valorDivida;
+    }
+
+    //Getter - Setter
+
+    public double getValorCredito() {
+        return valorCredito;
+    }
+
+    public void setValorCredito(double valorCredito) {
+        this.valorCredito = valorCredito;
+    }
+
+    public double getValorDivida() {
+        return valorDivida;
+    }
+
+    public void setValorDivida(double valorDivida) {
+        this.valorDivida = valorDivida;
+    }
+
+    //métodos
+    public void obterSaldo(){
+        double saldo = valorCredito - valorDivida;
+        return ;
+    }
+}
+
