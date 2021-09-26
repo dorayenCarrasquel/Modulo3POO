0932af6fdb538d6b133b34b81fb3a3665abdf367
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-09-26 15:13:22 -0300

    Creando Relação de Herança y Metodos Base na Clase Professores

---
+++ src/br/com/zup/ClassePolimorfismo/Professores.java
@@ -0,0 +1,16 @@
+package br.com.zup.ClassePolimorfismo;
+
+public class Professores  extends Funcionarios{
+    //Atributos
+    private String nivelDeGraduacao;
+    private String dsciplinaMinistrada;
+    private int quantidadeDeAlunos;
+    private int quantidadeDeTurmas;
+
+    //Construtores
+    public Professores(){
+
+    }
+
+
+}
