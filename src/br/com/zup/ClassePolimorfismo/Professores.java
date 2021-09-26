0f790cecb4f37d29113a8a39026473f3ffbde916
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-09-26 15:19:03 -0300

    Getters and Setter Classe Professores

--- src/br/com/zup/ClassePolimorfismo/Professores.java
+++ src/br/com/zup/ClassePolimorfismo/Professores.java
@@ -1,6 +1,6 @@
 package br.com.zup.ClassePolimorfismo;
 
-public class Professores  extends Funcionarios{
+public class Professores extends Funcionarios {
     //Atributos
     private String nivelDeGraduacao;
     private String dsciplinaMinistrada;
@@ -8,9 +8,52 @@ public class Professores  extends Funcionarios{
     private int quantidadeDeTurmas;
 
     //Construtores
-    public Professores(){
+    public Professores() {
 
     }
+
+    public Professores(String nome, String cpf, String numeroDeRegistro, String orgaoDeLotacao, double salario, String nivelDeGraduacao, String dsciplinaMinistrada, int quantidadeDeAlunos, int quantidadeDeTurmas) {
+        super(nome, cpf, numeroDeRegistro, orgaoDeLotacao, salario);
+        this.nivelDeGraduacao = nivelDeGraduacao;
+        this.dsciplinaMinistrada = dsciplinaMinistrada;
+        this.quantidadeDeAlunos = quantidadeDeAlunos;
+        this.quantidadeDeTurmas = quantidadeDeTurmas;
+    }
+
+    //Getter & Setters
+
+    public String getNivelDeGraduacao() {
+        return nivelDeGraduacao;
+    }
+
+    public void setNivelDeGraduacao(String nivelDeGraduacao) {
+        this.nivelDeGraduacao = nivelDeGraduacao;
+    }
+
+    public String getDsciplinaMinistrada() {
+        return dsciplinaMinistrada;
+    }
+
+    public void setDsciplinaMinistrada(String dsciplinaMinistrada) {
+        this.dsciplinaMinistrada = dsciplinaMinistrada;
+    }
+
+    public int getQuantidadeDeAlunos() {
+        return quantidadeDeAlunos;
+    }
+
+    public void setQuantidadeDeAlunos(int quantidadeDeAlunos) {
+        this.quantidadeDeAlunos = quantidadeDeAlunos;
+    }
+
+    public int getQuantidadeDeTurmas() {
+        return quantidadeDeTurmas;
+    }
+
+    public void setQuantidadeDeTurmas(int quantidadeDeTurmas) {
+        this.quantidadeDeTurmas = quantidadeDeTurmas;
+    }
+
 
 
 }
