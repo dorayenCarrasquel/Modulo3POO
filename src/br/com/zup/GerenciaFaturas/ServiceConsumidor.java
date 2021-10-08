41bc84cc17db9137c9c6e57110a99ade8c517911
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-10-08 09:34:52 -0300

    Validar email no cadastro

--- src/br/com/zup/GerenciaFaturas/ServiceConsumidor.java
+++ src/br/com/zup/GerenciaFaturas/ServiceConsumidor.java
@@ -8,10 +8,14 @@ public class ServiceConsumidor {
 
     public static Consumidor cadastrarConsumidor(String nome, String email, String tipoPessoa) throws Exception {
         TipoConsumidor pessoavalidado = validarTipoDeConsumidor(tipoPessoa);
+        validarEmail(email);
+        verificarEmailRepetido(email);
         Consumidor cliente = new Consumidor(nome, email, pessoavalidado);
         consumidores.add(cliente);
         return cliente;
     }
+
+
 
     public static TipoConsumidor validarTipoDeConsumidor(String tipoDeConsumidor) throws Exception {
         for (TipoConsumidor tipoReferencia : TipoConsumidor.values()) {
@@ -23,12 +27,29 @@ public class ServiceConsumidor {
         throw new Exception("Tipo de Consumidor não Valido. ");
     }
 
-    public static Consumidor pegarConsumidor(String email) throws Exception{
-        for (Consumidor emailReferencia :consumidores) {
-            if(email.equalsIgnoreCase(emailReferencia.getEmail())) {
+    public static Consumidor pegarConsumidor(String email) throws Exception {
+        for (Consumidor emailReferencia : consumidores) {
+            if (email.equalsIgnoreCase(emailReferencia.getEmail())) {
                 return emailReferencia;
             }
-        }throw new Exception("Email não cadastrado. ");
+        }
+        throw new Exception("Email não cadastrado. ");
+    }
+
+    public static void verificarEmailRepetido(String email) throws Exception {
+        for (Consumidor emailReferencia : consumidores) {
+            if (emailReferencia.getEmail().equals(email)) {
+                throw new Exception("E-mail repetido");
+            }
+        }
+    }
+
+    public static void validarEmail(String email) throws Exception {
+        for (Consumidor emailReferencia : consumidores) {
+            if (!email.contains("@")) {
+                throw new Exception("O e-mail não é valido");
+            }
+        }
     }
 
 
